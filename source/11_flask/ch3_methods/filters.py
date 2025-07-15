8c7551025f37b2cde283698efcedee911e7c67cd
k-j-hyun <spellrain@naver.com>
2025-07-15 18:04:15 +0900

    '250715RESTAPI'

--- source/11_flask/ch3_methods/filters.py
+++ source/11_flask/ch3_methods/filters.py
@@ -1,6 +1,6 @@
 def mask_password(password):
-    return '*' * len(password)
+  return "*" * len(password)
 
-if __name__ == "__main__":
-    password = input("비밀번호 : ")
-    print(mask_password(password))  # 비밀번호를 마스킹 처리하여 출력
+if __name__=="__main__":
+  password = input('비밀번호')
+  print(mask_password(password))
